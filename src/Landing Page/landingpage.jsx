a0e9f3d15b629af2a0be5604b4870de869328b02
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-08-23 17:07:11 +0530

    Creating email input box

--- src/Landing Page/landingpage.jsx
+++ src/Landing Page/landingpage.jsx
@@ -10,8 +10,8 @@ export default function Landingpage() {
             <div className="webbody">
                 <div className="jdjkkvm">
                     <div className="kdkvmmv">
-                        <img src="https://g1uudlawy6t63z36.public.blob.vercel-storage.com/Favicon%20Image/logo.webp" alt="" height={"50px"} style={{ marginLeft: "50px" }} className='laptop'/>
-                        
+                        <img src="https://g1uudlawy6t63z36.public.blob.vercel-storage.com/Favicon%20Image/logo.webp" alt="" height={"50px"} style={{ marginLeft: "50px" }} className='laptop' />
+
                     </div>
                     <div className="buttons">
                         <Link style={{ color: "black", textDecoration: "none" }} className='jnvmn'>
@@ -30,20 +30,26 @@ export default function Landingpage() {
                 </div>
                 <div className="body">
                     <div className="text">
-                    <Typewriter
-                                    options={{
-                                        strings: ['Start your coding<br/>journey today ', 'Code Your Way<br/>to Success', 'Unlock Your Coding<br/>Potential', 'Embark on Your<br/>Coding Odyssey', 'Ignite Your Passion<br/>for Coding'],
-                                        autoStart: true,
-                                        loop: true,
-                                        cursor: '|',
-                                        delay: 75,
-                                    }}
-                                />
-                    <div className="jhjkfv">
-                    Learn to code from scratch with job focussed courses designed by experts.
+                        <Typewriter
+                            options={{
+                                strings: ['Start your coding<br/>journey today ', 'Code Your Way<br/>to Success', 'Unlock Your Coding<br/>Potential', 'Embark on Your<br/>Coding Odyssey', 'Ignite Your Passion<br/>for Coding'],
+                                autoStart: true,
+                                loop: true,
+                                cursor: '|',
+                                delay: 75,
+                            }}
+                        />
+                        <div className="jhjkfv">
+                            Learn to code from scratch with job focussed courses designed by experts.
+                        </div>
+                        <div className="jshnsf">
+                            <input type="text" placeholder='Enter email' className='signupemail'/>
+                            <div className="mhdjddbdn">
+                                Starting Learning Today
+                            </div>
+                        </div>
                     </div>
-                    </div>
-                    <img src="https://g1uudlawy6t63z36.public.blob.vercel-storage.com/Coding_Boy" alt="" className='codingboy'/>
+                    <img src="https://g1uudlawy6t63z36.public.blob.vercel-storage.com/Coding_Boy" alt="" className='codingboy' />
                 </div>
             </div>
         </>
