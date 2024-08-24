610c35cddd6a8035e59a835a8b76b9523d55a173
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-08-24 18:41:45 +0530

    vercel speed analysis added

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import {
   Route
 } from 'react-router-dom';
 import Landingpage from './Landing Page/landingpage';
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 function App() {
   return (
     <BrowserRouter>
     <Routes>
         <Route path="/" element={<Landingpage />} />
       </Routes>
+      <SpeedInsights />
       <Analytics />
     </BrowserRouter>
   );
