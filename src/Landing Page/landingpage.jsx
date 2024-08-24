f2fbb5ea57e2b196562d66753e003faa38e32c51
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-08-24 12:23:28 +0530

    css change

--- src/Landing Page/landingpage.jsx
+++ src/Landing Page/landingpage.jsx
@@ -50,61 +50,95 @@ export default function Landingpage() {
                     </div>
                 </div>
                 <div className="bodyimage">
-                    <img src={currentImage} alt="" className='bodyimg'/>
+                    <img src={currentImage} alt="" className='bodyimg' />
                 </div>
                 <div className="dhifjkfjlf">
-                    <div className="items">
-                        <img src="https://images.vegnonveg.com/resized/700X573/11428/air-jordan-4-retro-blackwhite-black-66c718d11e771.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
-                    </div>
-                    <div className="items">
-                    <img src="https://images.vegnonveg.com/resized/700X573/11473/air-max-90-drift-action-greenblack-summit-white-green-66c6f5a6b95b7.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
-                    </div>
-                    <div className="items">
-                    <img src="https://images.vegnonveg.com/resized/700X573/11478/cortez-blackdark-smoke-grey-black-66c719e4c838e.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
-                    </div>
-                    <div className="items">
-                    <img src="https://images.vegnonveg.com/resized/700X573/11475/air-force-1-07-whitedragon-red-white-white-66c717cce8961.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
-                    </div>
-                    <div className="items">
-                    <img src="https://images.vegnonveg.com/resized/700X573/11477/killshot-2-phantomuniversity-gold-gum-medium-brown-multicolor-66c719921548d.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
-                    </div>
-                    <div className="items">
-                    <img src="https://images.vegnonveg.com/resized/700X573/11417/gel-quantum-360-viii-blackfiery-red-black-66b3610d4657e.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
-                    </div>
-                    <div className="items">
-                    <img src="https://images.vegnonveg.com/resized/700X573/11418/gel-quantum-360-viii-illuminate-yellowblack-black-66b3621140516.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
-                    </div>
-                    <div className="items">
-                    <img src="https://images.vegnonveg.com/resized/700X573/11426/air-jordan-1-mid-saillight-dew-muslin-white-66bb4a42e2b7a.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
-                    </div>
-                    <div className="items">
-                    <img src="https://images.vegnonveg.com/resized/700X573/11434/dunk-low-nn-baroque-brownblack-white-sail-brown-66bb4b2dc3a2a.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
-                    </div>
-                    <div className="items">
-                    <img src="https://images.vegnonveg.com/resized/700X573/11415/crazy-1-magic-beigeshadow-brown-brown-66b472e580b2e.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
-                    </div>
-                    <div className="items">
-                    <img src="https://images.vegnonveg.com/resized/700X573/11422/air-jordan-4-retro-se-smoke-greyiron-grey-cement-grey-grey-66b48d859813d.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
-                    </div>
-                    <div className="items">
-                    <img src="https://images.vegnonveg.com/resized/700X573/11396/dunk-low-retro-whitedenim-turq-white-66b3660dc7ed0.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
-                    </div>
-                    <div className="items">
-                    <img src="https://images.vegnonveg.com/resized/700X573/11406/dunk-low-coconut-milkflax-sail-brown-66b497326da01.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
-                    </div>
-                    <div className="items">
-                    <img src="https://images.vegnonveg.com/resized/700X573/11421/air-max-1-whitelight-army-neutral-grey-black-white-66b47248b5e8d.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
-                    </div>
-                    <div className="items">
-                    <img src="https://images.vegnonveg.com/resized/700X573/11416/gazelle-indoor-preloved-inkwonder-clay-sand-strata-blue-66b47456555a2.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
-                    </div>
-                    <div className="items">
-                    <img src="https://images.vegnonveg.com/resized/400X328/11476/v2k-run-summit-whitechrome-white-light-orewood-brown-white-66c71880235e9.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
-                    </div>
+                    <Link>
+                        <div className="items">
+                            <img src="https://images.vegnonveg.com/resized/700X573/11428/air-jordan-4-retro-blackwhite-black-66c718d11e771.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
+                        </div>
+                    </Link>
+                    <Link>
+                        <div className="items">
+                            <img src="https://images.vegnonveg.com/resized/700X573/11473/air-max-90-drift-action-greenblack-summit-white-green-66c6f5a6b95b7.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
+                        </div>
+                    </Link>
+                    <Link>
+                        <div className="items">
+                            <img src="https://images.vegnonveg.com/resized/700X573/11478/cortez-blackdark-smoke-grey-black-66c719e4c838e.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
+                        </div>
+                    </Link>
+                    <Link>
+                        <div className="items">
+                            <img src="https://images.vegnonveg.com/resized/700X573/11475/air-force-1-07-whitedragon-red-white-white-66c717cce8961.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
+                        </div>
+                    </Link>
+                    <Link>
+                        <div className="items">
+                            <img src="https://images.vegnonveg.com/resized/700X573/11477/killshot-2-phantomuniversity-gold-gum-medium-brown-multicolor-66c719921548d.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
+                        </div>
+                    </Link>
+                    <Link>
+                        <div className="items">
+                            <img src="https://images.vegnonveg.com/resized/700X573/11417/gel-quantum-360-viii-blackfiery-red-black-66b3610d4657e.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
+                        </div>
+                    </Link>
+                    <Link>
+                        <div className="items">
+                            <img src="https://images.vegnonveg.com/resized/700X573/11418/gel-quantum-360-viii-illuminate-yellowblack-black-66b3621140516.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
+                        </div>
+                    </Link>
+                    <Link>
+                        <div className="items">
+                            <img src="https://images.vegnonveg.com/resized/700X573/11426/air-jordan-1-mid-saillight-dew-muslin-white-66bb4a42e2b7a.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
+                        </div>
+                    </Link>
+                    <Link>
+                        <div className="items">
+                            <img src="https://images.vegnonveg.com/resized/700X573/11434/dunk-low-nn-baroque-brownblack-white-sail-brown-66bb4b2dc3a2a.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
+                        </div>
+                    </Link>
+                    <Link>
+                        <div className="items">
+                            <img src="https://images.vegnonveg.com/resized/700X573/11415/crazy-1-magic-beigeshadow-brown-brown-66b472e580b2e.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
+                        </div>
+                    </Link>
+                    <Link>
+                        <div className="items">
+                            <img src="https://images.vegnonveg.com/resized/700X573/11422/air-jordan-4-retro-se-smoke-greyiron-grey-cement-grey-grey-66b48d859813d.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
+                        </div>
+                    </Link>
+                    <Link>
+                        <div className="items">
+                            <img src="https://images.vegnonveg.com/resized/700X573/11396/dunk-low-retro-whitedenim-turq-white-66b3660dc7ed0.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
+                        </div>
+                    </Link>
+                    <Link>
+                        <div className="items">
+                            <img src="https://images.vegnonveg.com/resized/700X573/11406/dunk-low-coconut-milkflax-sail-brown-66b497326da01.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
+                        </div>
+                    </Link>
+                    <Link>
+                        <div className="items">
+                            <img src="https://images.vegnonveg.com/resized/700X573/11421/air-max-1-whitelight-army-neutral-grey-black-white-66b47248b5e8d.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
+                        </div>
+                    </Link>
+                    <Link>
+                        <div className="items">
+                            <img src="https://images.vegnonveg.com/resized/700X573/11416/gazelle-indoor-preloved-inkwonder-clay-sand-strata-blue-66b47456555a2.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
+                        </div>
+                    </Link>
+                    <Link>
+                        <div className="items">
+                            <img src="https://images.vegnonveg.com/resized/400X328/11476/v2k-run-summit-whitechrome-white-light-orewood-brown-white-66c71880235e9.jpg" alt="" height={'100px'} width={'100px'} style={{ borderRadius: '50%' }} />
+                        </div>
+                    </Link>
                 </div>
-                <div className="jjehfjnfjd">
-                    <video src="https://cdn.shopify.com/videos/c/o/v/334b6fb92fea4292887dadba3afbc61b.mp4" autoPlay muted loop className='promotionalvideo'></video>
-                </div>
+                <Link>
+                    <div className="jjehfjnfjd">
+                        <video src="https://videos.pexels.com/video-files/8533112/8533112-uhd_2560_1440_25fps.mp4" autoPlay muted loop className='promotionalvideo'></video>
+                    </div>
+                </Link>
             </div>
         </>
     );
