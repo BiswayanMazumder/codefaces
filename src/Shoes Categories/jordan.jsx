457d96b1a2c129b7915329f564d0373dec59e3b6
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-08-26 20:04:52 +0530

    login link created

--- src/Shoes Categories/jordan.jsx
+++ src/Shoes Categories/jordan.jsx
@@ -192,7 +192,7 @@ export default function Jordan() {
                             <Link to="/dunks" style={{ textDecoration: "none", color: "black" }}>Dunks</Link>
                             <Link to="/airmax" style={{ textDecoration: "none", color: "black" }}>Air Max</Link>
                             <Link to="/slides" style={{ textDecoration: "none", color: "black" }}>Slides</Link>
-                            <Link to="/" style={{ textDecoration: "none", color: "black" }}>Login</Link>
+                            <Link to="/account/login" style={{ textDecoration: "none", color: "black" }}>Login</Link>
                         </div>
                     </div>
                 </div>
