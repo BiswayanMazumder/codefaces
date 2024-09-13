cc50095e46a01b3e097d4d488386e16318e936d4
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-09-13 12:02:03 +0530

    UI change

--- src/Landing Page/landingpage.jsx
+++ src/Landing Page/landingpage.jsx
@@ -59,7 +59,7 @@ export default function Landingpage() {
         <>
             <div className="webbody">
                 <div className="headersection">
-                    <div className="logo">
+                    {/* <div className="logo">
                         <div className="searchform">
                             <Link style={{ textDecoration: "none", color: "black" }} to={user ? "/" : '/account/login'}>
                                 <svg focusable="false" width="18" height="18" className="icon icon--header-search" viewBox="0 0 18 18">
@@ -83,10 +83,8 @@ export default function Landingpage() {
                                 </svg>
                             </Link>
                         </div>
-                        {/* <div className="logoimage">
-                            <img src="https://g1uudlawy6t63z36.public.blob.vercel-storage.com/_fa24086d-6873-4c24-9ff6-0aceb7380333-QyUF9bBdbH9jERIGwpyEPhaZ2HcKZL.jpg" alt="" className='logoimg' />
-                        </div> */}
-                    </div>
+                        
+                    </div> */}
                     <div className="headeroptions">
                         <div className="options">
                             {/* <Link to="/" style={{ textDecoration: "none", color: "black" }}>SS24</Link> */}
